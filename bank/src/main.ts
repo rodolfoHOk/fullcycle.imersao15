bfc555933f29623b74622fe9ad60d16222702562
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-10-19 16:12:48 -0300

    feat: add kafka consumers in transaction controller

--- bank/src/main.ts
+++ bank/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { PixKeyAlreadyExistsErrorFilter } from './pix-keys/filters/pix-key-already-exists-error.filter';
 import { PixKeyGrpcUnknownErrorFilter } from './pix-keys/filters/pix-key-grpc-unknown-error.filter';
 import { ValidationPipe } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +18,19 @@ async function bootstrap() {
       errorHttpStatusCode: 422,
     }),
   );
+
+  app.connectMicroservice({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['host.docker.internal:9094'],
+      },
+      consumer: {
+        groupId: 'transactions-consumer',
+      },
+    },
+  });
+  await app.startAllMicroservices();
 
   await app.listen(3000);
 }
--- bank/src/transactions/transactions.controller.ts
+++ bank/src/transactions/transactions.controller.ts
@@ -1,6 +1,16 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  ValidationPipe,
+} from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { CreateTransactionFromAnotherBankAccountDto } from './dto/create-transaction-from-another-bank-account.dto';
+import { ConfirmTransactionDto } from './dto/confirm-transaction.dto';
 
 @Controller('bank-accounts/:bankAccountId/transactions')
 export class TransactionsController {
@@ -17,5 +27,42 @@ export class TransactionsController {
   @Get()
   findAll(@Param('bankAccountId') bankAccountId: string) {
     return this.transactionsService.findAll(bankAccountId);
+  }
+
+  @MessagePattern('bank001')
+  async onTransactionProcessedBank001(
+    @Payload(new ValidationPipe())
+    message: CreateTransactionFromAnotherBankAccountDto | ConfirmTransactionDto,
+  ) {
+    if (process.env.BANK_CODE !== '001') return;
+    await this.processTransaction(message);
+  }
+
+  @MessagePattern('bank002')
+  async onTransactionProcessedBank002(
+    @Payload(new ValidationPipe())
+    message: CreateTransactionFromAnotherBankAccountDto | ConfirmTransactionDto,
+  ) {
+    if (process.env.BANK_CODE !== '002') return;
+    await this.processTransaction(message);
+  }
+
+  private async processTransaction(
+    message: CreateTransactionFromAnotherBankAccountDto | ConfirmTransactionDto,
+  ) {
+    try {
+      if (message.status === 'pending') {
+        await this.transactionsService.createFromAnotherBankAccount(
+          message as CreateTransactionFromAnotherBankAccountDto,
+        );
+      }
+      if (message.status === 'confirmed') {
+        await this.transactionsService.confirmTransaction(
+          message as ConfirmTransactionDto,
+        );
+      }
+    } catch (err) {
+      console.error(err);
+    }
   }
 }
