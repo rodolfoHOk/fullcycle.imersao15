b6be77e3a18fb93122f7e223e6c4515c10f9da33
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-10-19 11:38:19 -0300

    feat: fill bank account service

--- bank/src/bank-accounts/bank-accounts.controller.ts
+++ bank/src/bank-accounts/bank-accounts.controller.ts
@@ -1,15 +1,6 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { BankAccountsService } from './bank-accounts.service';
 import { CreateBankAccountDto } from './dto/create-bank-account.dto';
-import { UpdateBankAccountDto } from './dto/update-bank-account.dto';
 
 @Controller('bank-accounts')
 export class BankAccountsController {
@@ -27,19 +18,6 @@ export class BankAccountsController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.bankAccountsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateBankAccountDto: UpdateBankAccountDto,
-  ) {
-    return this.bankAccountsService.update(+id, updateBankAccountDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.bankAccountsService.remove(+id);
+    return this.bankAccountsService.findOne(id);
   }
 }
