741a90fb099317bdef072d0c18406348c9d2eba1
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-10-20 18:18:49 -0300

    feat: configs to run 2 instances

--- frontnext/src/components/ThemeRegistry/theme.ts
+++ frontnext/src/components/ThemeRegistry/theme.ts
@@ -1,5 +1,5 @@
 import { createTheme } from '@mui/material';
-import { blue } from '@mui/material/colors';
+import { blue, green } from '@mui/material/colors';
 import { Roboto } from 'next/font/google';
 
 const roboto = Roboto({
@@ -12,14 +12,42 @@ const theme = createTheme({
   palette: {
     mode: 'light',
     primary: {
-      main: blue[500],
-      light: blue[300],
-      dark: blue[700],
+      main:
+        process.env.NEXT_PUBLIC_BANK_CODE === '001' ? blue[500] : green[500],
+      light:
+        process.env.NEXT_PUBLIC_BANK_CODE === '001' ? blue[300] : green[300],
+      dark:
+        process.env.NEXT_PUBLIC_BANK_CODE === '001' ? blue[700] : green[700],
       contrastText: '#fff',
+    },
+    text: {
+      primary: '#707070',
     },
   },
   typography: {
     fontFamily: roboto.style.fontFamily,
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: (themeParam) => ({
+        body: {
+          backgroundColor: '#f5f5f5',
+          background: 'url("/img/back.png") repeat right top',
+          WebkitBackgroundSize: 'cover',
+          MozBackgroundSize: 'cover',
+          OBackgroundSize: 'cover',
+          backgroundSize: 'cover',
+        },
+      }),
+    },
+    //@ts-expect-error - MuiDataGrid is not part of the types
+    MuiDataGrid: {
+      styleOverrides: {
+        columnHeaderTitle: {
+          fontWeight: 'bold',
+        },
+      },
+    },
   },
 });
 
