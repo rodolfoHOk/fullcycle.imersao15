929085d1262eec5454ea60a6a2ccdeadf358d948
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-10-20 12:35:22 -0300

    feat: add fetch bank accounts in bank accounts page and add dev container config

--- frontnext/src/app/bank-accounts/page.tsx
+++ frontnext/src/app/bank-accounts/page.tsx
@@ -1,3 +1,16 @@
+import { BankAccount } from '@/models/models';
+
+async function getBankAccounts(): Promise<BankAccount[]> {
+  const nestApiUrl = process.env.NEXT_PUBLIC_NEST_API_URL;
+  const response = await fetch(`${nestApiUrl}/bank-accounts`, {
+    next: {
+      revalidate: 10,
+    },
+  });
+
+  return response.json();
+}
+
 export default function BankAccountsPage() {
   return (
     <div>
